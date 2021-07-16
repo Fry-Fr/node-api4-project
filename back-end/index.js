c649e6dbf75cfdccfa9e96621c615378dbc4b7d9
Fry-Fr <ryjlwdg89@gmail.com>
2021-07-15 21:27:26 -0400

    removed the sub-folder

--- back-end/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const server = require('./api/server'); // require your server and launch it
-const port = process.env.PORT || 4000
-
-server.listen(port, () => {
-    console.log(`\n * server running on http://localhost:4000 * \n`)
-});
