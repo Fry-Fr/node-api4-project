0d46ddc802aca9cf22039f83b5ae01709477f7b8
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-07-15 22:22:41 -0400

    changes

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./api/server'); // require your server and launch it
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-    console.log(`\n * server running on http://localhost:4000 * \n`)
+    console.log(`\n * server running on http://localhost:${port} * \n`)
 });
